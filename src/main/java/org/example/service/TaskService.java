c32d3acf2abcf0df461164da6e49066e79304f97
inondayan2003-cell <inondayan.2003@gmail.com>
2026-02-16 17:40:57 +0200

    new

---
+++ src/main/java/org/example/service/TaskService.java
@@ -0,0 +1,35 @@
+package org.example.service;
+
+import org.example.model.Task;
+import org.example.repo.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+
+    public Task saveTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
+    }
+
+
+    public Task updateTask(Task task) {
+        return taskRepository.save(task);
+    }
+}
